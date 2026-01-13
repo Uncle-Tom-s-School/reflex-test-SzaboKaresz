bc4d2609774d98cbf7e8e76f96f737eb38b0f416
Karoly <you@example.com>
2026-01-13 11:43:35 +0100

    gatya

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,39 @@
+import { useEffect, useState } from "react";
+
+
+function randomSzam() {
+  return Math.floor(Math.random() * 10);
+}
+
 const App = () => {
+  const [szam, setSzam] = useState(0);
+  const [waiting, setWaiting] = useState(false); 
+  const [ready, setReady] = useState(false);
+
+  function visszaSzamlalo(szam: number) {
+    while (szam > 0) {
+      setSzam(prev => prev - 1);
+      if (szam === 0) {
+        zoldre();
+      }
+    }
+  }
+  function zoldre() {
+    <style>
+      
+    </style>
+  }
   return (
-    <div>App</div>
+    <div>
+      <button onClick={() => {setSzam(randomSzam()); visszaSzamlalo(szam);}} className="tab" >Várj a zöld képernyőre</button>
+      <button onClick={() => { setWaiting(!waiting); setSzam(randomSzam()); }} >Indítás</button>
+    </div>
+    
+
+    
   )
 }
 
+
 export default App
+
